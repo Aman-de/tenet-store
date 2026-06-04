4d94e6ad8d8a719f18ea8597af2442959ad78aa4
aman <letmedoedit@gmail.com>
2026-06-04 17:11:29 +0530

    Wrap CartDrawer in Suspense to fix Next.js build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 
 import CartDrawer from "@/components/CartDrawer";
@@ -69,7 +70,9 @@ export default function RootLayout({
             <div className="lg:hidden">
               <MobileBottomNav />
             </div>
-            <CartDrawer />
+            <Suspense fallback={null}>
+              <CartDrawer />
+            </Suspense>
             <WishlistDrawer />
             <SpeedInsights />
             <Analytics />
